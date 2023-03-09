1ad7a39476e33f5568274e56758a8a40705b8c06
zzZx <106265664+zzZxiong@users.noreply.github.com>
2023-03-09 16:52:39 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/icon.jpg") {
+    myImage.setAttribute("src", "images/icon1.png");
+  } else {
+    myImage.setAttribute("src", "images/icon.jpg");
+  }
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
